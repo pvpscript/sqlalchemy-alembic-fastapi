f66a02e5d37199076021b2542e7035cdb73a2175
Pedro Vitor Piassa <33137099+pvpscript@users.noreply.github.com>
2023-06-05 21:38:36 -0300

    chore: updating session creation and usage

--- app/db/session.py
+++ app/db/session.py
@@ -1,5 +1,7 @@
 import os
 
+from asyncio import current_task
+
 from functools import lru_cache
 
 from typing import Any
@@ -7,23 +9,23 @@ from typing import Any
 from sqlalchemy import create_engine
 from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.ext.asyncio import async_sessionmaker
+from sqlalchemy.ext.asyncio import async_scoped_session
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.resources.config import Config
 
-engine = create_async_engine(Config.DATABASE_URI, pool_pre_ping=True, echo=True)
 
 @lru_cache
 def create_session() -> AsyncSession:
-    return async_sessionmaker(autocommit=False,
-                              autoflush=False,
-                              expire_on_commit=False,
-                              bind=engine)
+    engine = create_async_engine(Config.DATABASE_URI, pool_pre_ping=True, echo=True)
+    session = async_sessionmaker(autocommit=False,
+                                 autoflush=False,
+                                 expire_on_commit=False,
+                                 bind=engine)
 
-def get_session() -> Any:
+    return async_scoped_session(session, scopefunc=current_task)
+
+async def get_session() -> Any:
     AsyncSession = create_session()
-    try:
-        yield AsyncSession
-    finally:
-        #AsyncSession.remove()
-        pass
+    async with AsyncSession() as async_session:
+        yield async_session

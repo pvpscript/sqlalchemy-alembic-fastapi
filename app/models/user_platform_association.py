0d0eceb9a12cb393a1e08489799f1fce579fd192
Pedro Vitor Piassa <33137099+pvpscript@users.noreply.github.com>
2023-06-05 21:32:43 -0300

    chore: using python class for association table

--- app/models/user_platform_association.py
+++ app/models/user_platform_association.py
@@ -1,19 +1,29 @@
 import sqlalchemy as sa
 
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column 
+
+from dataclasses import dataclass
+from uuid import UUID
+from datetime import datetime
+
 from app.db.base_class import Base
 
-association_table = sa.Table(
-    'user_platform_association',
-    Base.metadata,
-    sa.Column(
-        'user_id',
-        sa.ForeignKey('user.id'),
-        primary_key=True,
-    ),
-    sa.Column(
-        'platform_id',
-        sa.ForeignKey('platform.id'),
-        primary_key=True,
-    ),
-)
 
+@dataclass
+class UserPlatformAssociation(Base):
+    __tablename__ = 'user_platform_association'
+
+    user_id: Mapped[UUID] = mapped_column(sa.UUID, sa.ForeignKey('user.id'), primary_key=True)
+    platform_id: Mapped[UUID] = mapped_column(sa.UUID, sa.ForeignKey('platform.id'), primary_key=True)
+
+    created_at: datetime = sa.Column(
+        sa.TIMESTAMP(timezone=True),
+        nullable=False,
+        server_default=sa.func.now()
+    )
+    modified_at: datetime = sa.Column(
+        sa.TIMESTAMP(timezone=True),
+        nullable=False,
+        server_default=sa.func.now()
+    )

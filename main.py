70a1959cef7b4e29a9c7f2f4091004b805a8588d
Pedro Vitor Piassa <33137099+pvpscript@users.noreply.github.com>
2023-06-05 21:40:31 -0300

    feat: adding another test for the newer database

--- main.py
+++ main.py
@@ -5,9 +5,15 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from app.schemas.user import User, UserCreate, UserUpdate
 from app.schemas.platform import Platform, PlatformCreate, PlatformUpdate
+from app.schemas.user_platform import UserPlatform, UserPlatformCreate
+from app.schemas.document import Document, DocumentCreate, DocumentUpdate
+from app.schemas.asset import Asset, AssetCreate, AssetUpdate
 
 from app.db.services.user import UserService, get_user_service
 from app.db.services.platform import PlatformService, get_platform_service
+from app.db.services.user_platform_association import UserPlatformAssociationService, get_user_platform_association_service
+from app.db.services.document import DocumentService, get_document_service
+from app.db.services.asset import AssetService, get_asset_service
 
 from typing import List
 from uuid import UUID
@@ -68,10 +74,72 @@ async def delete(platform_id: UUID, platform_service: PlatformService = Depends(
 
 
 
+testing_router = APIRouter(prefix='/testing', tags=['testing'])
+
+from pydantic import BaseModel
+class UserPlatformsPayload(BaseModel):
+    user: User
+    platforms: List[Platform]
+
+class PlatformUsersPayload(BaseModel):
+    users: List[User]
+    platform: Platform
+
+class TestPayload(BaseModel):
+    user: UserCreate
+    platform: PlatformCreate
+    document: DocumentCreate
+    assets: List[AssetCreate]
+
+@testing_router.post("/")
+async def test(payload: TestPayload,
+               user_service: UserService = Depends(get_user_service),
+               platform_service: PlatformService = Depends(get_platform_service),
+               user_platform_association_service: UserPlatformAssociationService = Depends(get_user_platform_association_service),
+               document_service: DocumentService = Depends(get_document_service),
+               asset_service: AssetService = Depends(get_asset_service)):
+    user = await user_service.save(payload.user)
+    print("----------------------------------------------------------------------------------------------------")
+    print(user)
+    print("----------------------------------------------------------------------------------------------------")
+    platform = await platform_service.save(payload.platform)
+    print("----------------------------------------------------------------------------------------------------")
+    print(platform)
+    print("----------------------------------------------------------------------------------------------------")
+
+    user_platform_create = UserPlatformCreate(user_id=user.id,
+                                              platform_id=platform.id)
+    user_platform = await user_platform_association_service.save(user_platform_create)
+    print("----------------------------------------------------------------------------------------------------")
+    print(user_platform)
+    print("----------------------------------------------------------------------------------------------------")
+
+    updated_document = payload.document.copy(update={'user_id': user.id})
+    document = await document_service.save(updated_document)
+    print("----------------------------------------------------------------------------------------------------")
+    print(document)
+    print("----------------------------------------------------------------------------------------------------")
+    for payload_asset in payload.assets:
+        updated_asset = payload_asset.copy(update={'user_id': user.id})
+        asset = await asset_service.save(updated_asset)
+        print("----------------------------------------------------------------------------------------------------")
+        print(asset)
+        print("----------------------------------------------------------------------------------------------------")
+
+    result_user = await user_service.get_by_id(user.id)
+    print("----------------------------------------------------------------------------------------------------")
+    print(result_user)
+
+
+    
+
+
+
 app = FastAPI()
 
 app.include_router(user_router)
 app.include_router(platform_router)
+app.include_router(testing_router)
 
 
 if __name__ == "__main__":

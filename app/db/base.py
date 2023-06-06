bed6eedfde9a9b6baab65f1586ddf1824624c51c
Pedro Vitor Piassa <33137099+pvpscript@users.noreply.github.com>
2023-06-05 21:37:19 -0300

    chore: importing newer modules

--- app/db/base.py
+++ app/db/base.py
@@ -4,4 +4,7 @@ from app.db.base_class import Base
 # Model classes
 from app.models.user import User
 from app.models.platform import Platform
-from app.models.user_platform_association import association_table
+from app.models.user_platform_association import UserPlatformAssociation
+
+from app.models.document import Document
+from app.models.asset import Asset
